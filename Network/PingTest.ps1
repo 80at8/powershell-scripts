bc8d4d40ac00a1534f261e398e62f0cd56fd49be
andrew <andy@sonar.software>
2020-04-30 23:54:59 -0600

    uploaded pingtester

---
+++ Network/PingTest.ps1
@@ -0,0 +1,22 @@
+﻿param (
+    [string]$TargetName
+)
+
+[bool]$StateSwitch
+$StateSwitch = $true
+
+# Include some code in here to work across powershell versions.
+
+while ($true) {
+    $PingStatus = Test-Connection -ComputerName $TargetName -Count 3 -Quiet
+    $TimeStamp = Get-Date
+    if ($PingStatus -eq $false -And $StateSwitch -eq $true) {
+        Write-Host "State Changed from Online to Offline at: "  $TimeStamp
+        $StateSwitch = $false
+    }
+    if ($PingStatus -eq $true -And $StateSwitch -eq $false) {
+        Write-Host "State Changed from Offline to Online at: "  $TimeStamp
+        $StateSwitch = $true
+    }
+
+}
